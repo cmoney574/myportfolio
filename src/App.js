364ba125d02a8c3c6d9048e0298064230c78a8d9
Charlie Money <cmoney5748@gmail.com>
2022-02-27 22:21:39 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import AboutMe from './components/AboutMe';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="portfolio-app">
+      <Navbar />
       <AboutMe />
     </div>
   );
