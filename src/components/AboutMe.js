0df76b939849f78c118e9b6c67f3a406da1b0051
Charlie Money <cmoney5748@gmail.com>
2022-02-27 22:13:10 -0500

    added sbout me words

---
+++ src/components/AboutMe.js
@@ -0,0 +1,20 @@
+export default function AboutMe() {
+    return (
+        <section id="about">
+            <div>
+                <div>
+                    <h2> My name is Charles Money</h2>
+                    <h3> I can make my own website </h3>                
+                    <p> 
+                        This is quite interesting
+                    </p>
+                    <div>
+                        <a href="#contact">Contact Me</a>
+                        <a href="#projects">Projects</a>
+                        <a href="#Resume">Resume</a>
+                    </div>
+                </div>
+            </div>   
+        </section>
+    );
+}
